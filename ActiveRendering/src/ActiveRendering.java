967c48a00bda31004283c2691905c2b5f1f4a8be
g770 <g770@users.noreply.github.com>
2016-08-08 19:58:45 -0700

    add active rendering example

---
+++ ActiveRendering/src/ActiveRendering.java
@@ -0,0 +1,94 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+
+/**
+ * Created by doug on 8/8/2016.
+ */
+public class ActiveRendering extends JFrame implements Runnable {
+
+    private FrameRate frameRate;
+    private BufferStrategy bs;
+    private volatile boolean running;
+    private Thread gameThread;
+
+    public ActiveRendering() {
+        frameRate = new FrameRate();
+    }
+
+    protected void createAndShowGUI() {
+        Canvas canvas = new Canvas();
+        canvas.setSize(320, 240);
+        canvas.setBackground(Color.BLACK);
+        canvas.setIgnoreRepaint(true);
+        getContentPane().add(canvas);
+        setTitle("Active Rendering");
+        setIgnoreRepaint(true);
+        pack();
+
+        setVisible(true);
+        canvas.createBufferStrategy(2);
+        bs = canvas.getBufferStrategy();
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void run() {
+        running = true;
+        frameRate.initialize();
+        while(running) {
+            gameLoop();
+        }
+    }
+
+    public void gameLoop() {
+        do {
+            do {
+                Graphics g = null;
+                try {
+                    g = bs.getDrawGraphics();
+                    g.clearRect(0, 0, getWidth(), getHeight());
+                    render(g);
+                } finally {
+                    if (g != null) {
+                        g.dispose();
+                    }
+                }
+            } while(bs.contentsRestored());
+            bs.show();
+        } while (bs.contentsLost());
+    }
+
+    private void render(Graphics g) {
+        frameRate.calculate();
+        g.setColor(Color.GREEN);
+        g.drawString(frameRate.getFrameRate(), 30, 30);
+    }
+
+    protected void onWindowClosing() {
+        try {
+            running = false;
+            gameThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        final ActiveRendering app = new ActiveRendering();
+        app.addWindowListener(new WindowAdapter() {
+                                  @Override
+                                  public void windowClosing(WindowEvent e) {
+                                      app.onWindowClosing();
+                                  }
+                              });
+
+                SwingUtilities.invokeLater(() -> app.createAndShowGUI());
+    }
+
+}
